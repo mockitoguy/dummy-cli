7594e62045419e086f7df61369585aa4c6d4d1cb
Szczepan Faber <sfaber@linkedin.com>
2020-03-02 19:41:01 -0800

    Format

--- src/main/java/com/mockitoguy/Main.java
+++ src/main/java/com/mockitoguy/Main.java
@@ -2,12 +2,9 @@ package com.mockitoguy;
 
 /**
  * Hello world!
- *
  */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
